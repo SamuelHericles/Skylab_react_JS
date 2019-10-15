bf5840dffe6b584a02879f43051a4958588e90dc
SamuelHericles <samuel.herilces@alu.ufc.br>
2019-10-14 21:33:50 -0300

    Iniciando o curso de ReactJS

---
+++ ReactJS/huntweb/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header'
+import './style.css'
+const App = () => (
+    <div className='App'>
+      <Header/>
+    </div>
+)
+
+
+
+export default App;
